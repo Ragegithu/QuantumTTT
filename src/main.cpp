247ef231b2a2cca5dfbf4fc78a16a81956fc1cba
Ragegithu <147668478+Ragegithu@users.noreply.github.com>
2026-03-09 05:44:33 +0200

    Qubit struct and the Rz gate are built NICE!

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
+#include <cassert>
+#include <complex>
+
+using ComplexF = std::complex<float>;
+using ComplexD = std::complex<double>;
+
+struct qubit 
+{
+    ComplexF alpha = {1.0,0.0}; 
+    ComplexF beta = {0.0,0.0};
+
+    bool measured = false;
+
+    float probA()
+    {
+        return alpha.real() * alpha.real() + alpha.imag() * alpha.imag();
+    }
+    
+    float probB()
+    {
+        return beta.real() * beta.real() + beta.imag() * beta.imag();
+    }
+};
+
+void Rz(float angle, qubit& q)
+{
+    float c = cos(angle /2);
+    float s = sin(angle /2);
+
+    q.alpha = ComplexF(c,-s) * q.alpha;
+    q.beta =  ComplexF(c,+s) * q.beta;
+}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800,800}),"QTTT");
 
-    
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    shape.setPosition({300.f, 200.f});
+    qubit q;
+    std::cout << "Q1 P Alpha: " << q.probA() << std::endl;
+    std::cout << "Q1 P Beta: " << q.probB() << std::endl;
+    Rz(180,q);
+    std::cout << "Q1 P Alpha: " << q.probA() << std::endl;
+    std::cout << "Q1 P Beta: " << q.probB() << std::endl;
 
     while (window.isOpen())
     {
@@ -18,7 +54,6 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
         window.display();
     }
 }
