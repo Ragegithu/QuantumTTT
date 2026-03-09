b96b53a2125370d83bf6a3c50e5c24f0c262f0c8
Ragegithu <147668478+Ragegithu@users.noreply.github.com>
2026-03-09 08:41:55 +0200

    Ry and Hadamard are DONE! no docs on them yet wait a bit

--- src/main.cpp
+++ src/main.cpp
@@ -27,23 +27,52 @@ struct qubit
 
 void Rz(float angle, qubit& q)
 {
-    float c = cos(angle /2);
-    float s = sin(angle /2);
+    float rad = angle * (M_PI / 180);
+    float c = cos(rad /2);
+    float s = sin(rad /2);
 
     q.alpha = ComplexF(c,-s) * q.alpha;
     q.beta =  ComplexF(c,+s) * q.beta;
+}
+
+void Ry(float angle, qubit& q)
+{
+    float rad = angle * (M_PI / 180);
+    float c = cos(rad/2);
+    float s = sin(rad/2);
+
+    ComplexF newA = c * q.alpha - s * q.beta;
+    ComplexF newB = c * q.beta + s * q.alpha;
+
+    q.alpha = newA;
+    q.beta = newB;
+}
+
+void Hadamard(qubit& q)
+{
+    float s12 = 1/sqrt(2);
+    ComplexF newA = s12 * q.alpha + s12 * q.alpha;
+    ComplexF newB = s12 * q.beta - s12 * q.beta;
 }
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800,800}),"QTTT");
 
+    //just testing
     qubit q;
     std::cout << "Q1 P Alpha: " << q.probA() << std::endl;
     std::cout << "Q1 P Beta: " << q.probB() << std::endl;
+    Hadamard(q);
     Rz(180,q);
+    Ry(45,q);
+    Rz(180,q);
+    Ry(170,q);
+    Hadamard(q);
     std::cout << "Q1 P Alpha: " << q.probA() << std::endl;
     std::cout << "Q1 P Beta: " << q.probB() << std::endl;
+
+    //be sure to remove the above block pls :)
 
     while (window.isOpen())
     {
